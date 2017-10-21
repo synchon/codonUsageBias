62f6081260af4ec2d2c0069391f3e4a1ddf16d3c
Synchon Mandal <synchonmandal@gmail.com>
2017-10-21 15:04:28 +0530

    Updated source code

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 '''
 © 2017 Synchon Mandal
 '''
-
-import sys, os, getopt, textwrap, csv
+import os, argparse, textwrap, csv
 from operator import itemgetter
 from Bio import Entrez, SeqIO
 from Bio.SeqRecord import SeqRecord
@@ -13,55 +12,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # global variables
-root_path = os.getcwd()
-email_id = ""
-accession_id = ""
-codon_list = ()
+PATH = os.getcwd()
+ACCESSION_ID = ""
+CODON_LIST = ()
 
-def usage():
-        print('Usage: ./main.py -a accession-number -e email-id\n')
-        print('-h --help               - check the available options\n')
-        print('-a --accession_id=id    - NCBI accession number\n')
-        print('-e --email_id=email     - NCBI registered email-id\n')
-        print('Example:\n')
-        print('./main.py -a 123ABC -e user@example.com\n')
-        sys.exit(0)
-
-def command_line_argument_check():
-    '''checks for command-line arguments and performs necessary operation'''
-    if not len(sys.argv) > 1 :
-         usage()
-
-    try:
-        opts, args = getopt.getopt(sys.argv[1:],'ha:e:',['help','accession_id=','email_id='])
-    except getopt.GetoptError as err:
-        sys.stderr.write(err)
-        usage()
-
-    for o,a in opts:
-        if o in ("-h","--help"):
-            usage()
-
-        elif o in ("-a","--accession_id"):
-            global accession_id
-            accession_id = a
-            write_protein_sequence_to_file(convert_DNA_to_protein(perform_search_for_accession_id(handle_id=accession_id)))
-            plot_graph_of_analysed_dataset(analyse_mutation_dataset(dataset_file=accession_id))
-
-        elif o in ("-e","--email_id"):
-            email_id = a
-
-        else:
-            assert False,"Unhandled Option"
-
-def perform_search_for_accession_id(handle_id):
+def perform_search_for_accession_id(handle_id, email_id):
     '''searches the local directory /input for the .fasta file (offline) and if not found falls back to the Entrez database (online)'''
-    global root_path
-    global email_id
 
-    if os.path.exists(root_path + f'/input/{accession_id}/{accession_id}.fasta'):
+    global PATH
+
+    if os.path.exists(PATH + f'/input/{handle_id}/{handle_id}.fasta'):
         # path change and data extraction (offline)
-        os.chdir(root_path + f'/input/{handle_id}')
+        os.chdir(PATH + f'/input/{handle_id}')
         seq_record = SeqIO.read(f'{handle_id}.fasta','fasta')
     else:
         # based on Entrez API (online)
@@ -74,34 +36,47 @@ def perform_search_for_accession_id(handle_id):
 
 def compute_codon(nucleotide):
     '''splits the DNA sequence into codons'''
+
     return tuple(textwrap.wrap(str(nucleotide),3))
     # ['AGAGAG...'] => ['AGA','GAG','...',...]
 
 def convert_DNA_to_protein(dna):
     '''translates the DNA to Protein'''
-    global codon_list
-    codon_list = compute_codon(dna)
+
+    global CODON_LIST
+    # Generates codons and stores it in a list for analysis
+    CODON_LIST = compute_codon(dna)
+
+    # Converts DNA to protein
     protein = SeqRecord(dna.translate())
-    # print(f'No. of codons: {len(protein)}')
+
     return protein
 
 def write_protein_sequence_to_file(protein):
     '''writes a fasta file in the /output directory'''
-    global root_path
-    global accession_id
-    outputPath = root_path + f'/output/{accession_id}'
+
+    global PATH
+    global ACCESSION_ID
+
+    outputPath = PATH + f'/output/{ACCESSION_ID}'
+
+    # Checks for output directory
     if not os.path.exists(outputPath):
         os.makedirs(outputPath)
-    os.chdir(root_path + f'/output/{accession_id}')
-    seq_record = SeqIO.write(protein,f'{accession_id.lower()}_output.fasta','fasta')
-    os.chdir(root_path)
+
+    os.chdir(PATH + f'/output/{ACCESSION_ID}')
+    SeqIO.write(protein, f'{ACCESSION_ID}_output.fasta','fasta')
+    os.chdir(PATH)
 
 def analyse_mutation_dataset(dataset_file):
     '''analyses the given mutation dataset and returns the analysed dataset list'''
-    global root_path
-    global codon_list
+
+    global PATH
+    global CODON_LIST
+
     # input path
-    os.chdir(root_path + f'/input/{dataset_file}/')
+    os.chdir(PATH + f'/input/{dataset_file}/')
+
     # CSV parsing
     with open(f'{dataset_file}.csv','r') as mutation_file:
         mutation_reader = csv.reader(mutation_file)
@@ -116,7 +91,7 @@ def analyse_mutation_dataset(dataset_file):
     # row-based operation on dataset
     for row in mutation_dataset:
         row[1] = int(row[1])
-        row.append(codon_list[row[1]])
+        row.append(CODON_LIST[row[1]])
         original_and_mutated_nucleotide = row[4] + row[3]
         row.append(original_and_mutated_nucleotide)
         original_and_mutated_nucleotide_list.append(original_and_mutated_nucleotide)
@@ -161,24 +136,37 @@ def analyse_mutation_dataset(dataset_file):
         #               ('Mutated Amino Acid', 'H')]),...]
 
     # changes path to the /output directory; no further directory change required
-    os.chdir(root_path + f'/output/{dataset_file}/')
+    os.chdir(PATH + f'/output/{dataset_file}/')
 
     return analysed_dataset
 
 def plot_graph_of_analysed_dataset(dataset_list):
     '''plots the required graphs for the information from the mutation dataset'''
-    global accession_id
+
+    global ACCESSION_ID
     # change it for different graph style; default: fivethirtyeight
     plt.style.use('fivethirtyeight')
 
     # only change the default values when needed
     analysed_dataframe = pd.DataFrame(data=dataset_list)
-    analysed_dataframe.to_csv(f'{accession_id}_output.csv', encoding='utf-8')
+    analysed_dataframe.to_csv(f'{ACCESSION_ID}_output.csv', encoding='utf-8')
     analysed_dataframe.plot(title='Dataset Analysis', grid=True, fontsize=10, figsize=(20,6), x=['Original Nucleotide','Mutated Nucleotide'], y='Occurences', kind='bar', width=0.9, alpha=0.75)
-    plt.savefig(f'{accession_id}.pdf',bbox_inches='tight')
+    plt.savefig(f'{ACCESSION_ID}.pdf',bbox_inches='tight')
 
 def main():
-    command_line_argument_check()
+    global ACCESSION_ID
+
+    # Command-line argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', type=str, help='NCBI accession number')
+    parser.add_argument('email', type=str, help='NCBI registered email-id')
+    args = parser.parse_args()
+
+    ACCESSION_ID = args.id
+
+    # Passing the arguments from command line and passing to the required functions
+    write_protein_sequence_to_file(convert_DNA_to_protein(perform_search_for_accession_id(handle_id=args.id, email_id=args.email)))
+    plot_graph_of_analysed_dataset(analyse_mutation_dataset(dataset_file=args.id))
 
 if __name__ == '__main__':
     main()
